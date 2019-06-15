599b82d4a2127f59c6c448b6c400fc0610b7c671
Tomasz Wylężek <tomwylezek@gmail.com>
2019-06-15 14:55:39 +0200

    Initial commt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi thereee XD XD XD  s  s  ss xd
         </p>
         <a
           className="App-link"
